b8da4f906c7576474f58ccd6269cfbe1e53ad2e0
Alexandre de Souza Eustaquio <alexeus.souza@gmail.com>
2022-09-08 09:10:56 -0300

    Criação de Views
    Criação de Views para Author

---
+++ BookStore/Controllers/AuthorController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BookStore.Controllers
+{
+    public class AuthorController : Controller
+    {
+        [Route("listar")]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("criar")]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [Route("editar/{id:int}")]
+        public ActionResult Edit(int Id)
+        {
+            return View();
+        }
+
+        [Route("excluir/{id:int}")]
+        public ActionResult Delete(int Id)
+        {
+            return View();
+        }
+    }
+}
